e7e25c570c16821da9668204e22623e1fe967dae
Syam kakarla <syamkakarla98@gmail.com>
2018-02-18 10:35:30 +0530

    First commit

---
+++ expectday.py
@@ -0,0 +1,9 @@
+# This is a snippet for finding the day .
+# INPUT FORMAT  : Date Month Year
+# OUTPUT FORMAT : Weekname
+
+import calendar
+d,m,y=map(int,input().split())
+print((calendar.day_name[calendar.weekday(y,m,d)]).upper())
+
+
